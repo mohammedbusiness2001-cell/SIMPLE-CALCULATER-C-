4194b5f4459886fdfb5691d9bcd549e9b4f2cd8e
mohammedbusiness2001-cell <mohammedbusiness2001@gmail.com>
2026-01-31 17:17:20 +0300

    Add project files.

---
+++ SIMPLE CALCULATER/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+
+class Calculator
+{
+    static void Main()
+    {
+      
+        Console.Write("Operation: ");
+        string input = Console.ReadLine();
+
+        try
+        {
+            DataTable table = new DataTable();
+            var result = table.Compute(input, "");
+
+            Console.WriteLine("Result = " + result);
+        }
+        catch
+        {
+            Console.WriteLine("Invalid operation!");
+        }
+
+        Console.ReadKey();
+    }
+}
